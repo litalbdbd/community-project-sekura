de4e5b3a0e62f4ddaa5dbf69154267fed02f44e7
litalbdbd <51334211+litalbdbd@users.noreply.github.com>
2020-04-01 10:41:55 +0300

    starting point

--- App.js
+++ App.js
@@ -24,6 +24,34 @@ import {
 
 // const store = createStore(rootReducer)
 
+/*
+redux should have two reducers (combineReducers)
+reducer1 = user
+  userId, name, profileImage
+reducer2 = posts
+  ids: ['1', '2'],
+  map: {
+    1: {
+		id: '1',
+		title: 'First Item',
+		subTitle: 'blabla',
+		text: `There are enough articles on this site to fill two books, so it can sometimes be daunting to know where to start. Below are what many consider to be my “greatest hits,” the articles that have been the most popular, the most shared, or had the greatest effect on readers’ lives.
+		I’ve listed the best four articles in five different categories below: Understanding Yourself, Emotional Intelligence, Life Purpose, Relationships, and Self-Discipline.`,
+		likes: 0,
+		imageUrl: 'https://dianasuemi.com/wp-content/uploads/2017/08/QwupPdD.png'
+	},
+	2: {
+		id: '2',
+		title: 'Second Item',
+		subTitle: 'blabla',
+		text: `There are enough articles on this site to fill two books, so it can sometimes be daunting to know where to start. Below are what many consider to be my “greatest hits,” the articles that have been the most popular, the most shared, or had the greatest effect on readers’ lives.
+		I’ve listed the best four articles in five different categories below: Understanding Yourself, Emotional Intelligence, Life Purpose, Relationships, and Self-Discipline.`,
+		likes: 0,
+		imageUrl: 'https://dianasuemi.com/wp-content/uploads/2017/08/QwupPdD.png'
+	}
+  }
+*/
+
 import {API} from './mocks/server';
 
 class App extends Component {
@@ -31,17 +59,22 @@ class App extends Component {
     super(props);
     this.state = {
       isLoading: true,
-      posts: []
+      // posts: []
     }
   }
   async componentDidMount() {
     API.resetData(); //reset data for hot-reloading 
+    await API.updatePostLikes('1', '1', 'like')
     const posts = await API.getPosts();
     this.setState({posts, isLoading: false});
   }
-  handleLikePress = () => {
-    console.log('do some logic for handling like press')
-    // this.props.dispatch(actions.updateLike())
+  handleLikePress = async () => {
+    // console.log('do some logic for handling like press')
+    // this.props.dispatch(actions.updateLike(postId))
+
+    // await API.updatePostLikes('1', '1', 'unlike')
+    // const posts = await API.getPosts();
+    // this.setState({posts, isLoading: false});
   }
   renderItem = ({ item }) => {
     return (
